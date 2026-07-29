e1c17acaa9646657f216a186c5a502a61326714d
erickpaine43 <ereickpaine43@gmail.com>
2026-07-29 00:48:40 -0400

    add API_URL

--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:7036/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function apiClient<T>(
   endpoint: string,
